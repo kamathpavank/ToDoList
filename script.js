cdddae20be9768048b352225b1e9cdc424b47902
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-07-22 17:48:29 +0530

    Added cancle option

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
-  $("button").click(function() {
+$("#button").click(function() {
     var txt1 = $("#new").val();
-    $("ol").append('<li>' +txt1+ '</li>');
+    $("ol").append('<li>' +txt1+' '+'<button id="cancle">x</button>'+ '</li>');
 
   });
+
+  $(document).on('click','button', function(){
+          $(this).parent().fadeOut('slow');
+        });
+// $('#cancle').click(function(){
+//   $(this).parent().fadeOut('slow');
+// })
+// var re= $('#cancle');
+// re.toggleClass('strike').fadeOut('slow');
